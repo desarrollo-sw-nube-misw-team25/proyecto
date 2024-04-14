ed9269c999804a5ce7b9a431b2adcccc506b5c64
veronica <v.escobar1@uniandes.edu.co>
2024-04-13 22:58:16 -0500

    Created Video model with its corresponding methods

---
+++ src/models/video_model.py
@@ -0,0 +1,32 @@
+import uuid
+from app import db, bcrypt
+from datetime import datetime
+
+
+class Video(db.Model):
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    filename = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.Enum('uploaded', 'processed', name='status_types'), default='uploaded')
+
+    def __repr__(self):
+        return '<Video %r>' % {self.filename}
+
+    def get_id(self):
+        return self.id
+
+    def get_filename(self):
+        return self.filename
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        # Validate the input status
+        valid_statuses = {'uploaded', 'processed'}
+        if status not in valid_statuses:
+            raise ValueError(f"Invalid status '{status}'. Valid statuses are {valid_statuses}.")
+        self.status = status
