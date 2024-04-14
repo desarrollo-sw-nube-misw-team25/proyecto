75c57aed036f48c6b9e0009edcab93a2023b4e95
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-04-14 12:01:34 -0500

    generalize the bath processing calling

--- batchProcessing/video_processing.py
+++ batchProcessing/video_processing.py
@@ -6,10 +6,10 @@ import subprocess
 app = Celery('video_processing', backend='redis://localhost', broker='redis://localhost')
 
 @app.task
-def process_video(video_path):
+def process_video(video_path,nombre_video):
     
     try:    
-        subprocess.run(['sudo', 'docker', 'run', '-v', f'{video_path}:/app/input', 'batch-processing', '/app/input/pruebaVideo.mp4'], check=True)
+        subprocess.run(['sudo', 'docker', 'run', '-v', f'{video_path}:/app/input', 'batch-processing', f'/app/input/{nombre_video}'], check=True)
         return "Video processing complete"
     
     except subprocess.CalledProcessError as e:
@@ -17,7 +17,7 @@ def process_video(video_path):
 
 if __name__ == "__main__":
     
-    result = process_video.delay('/home/pelucapreb/Descargas')
+    result = process_video.delay('/app/src/videos')
     print("Task ID:", result.id)
 
 
