c9c22767e57b071282fe698a9802cbe49436c4bc
shernandezf <s.hernandezf@uniandes.edu.co>
2024-04-14 00:07:10 -0500

    vero pointed out a few mistakes my code had, thanks co-worker

--- batchProcessing/video_processing.py
+++ batchProcessing/video_processing.py
@@ -1,22 +1,18 @@
 
 
 from celery import Celery
-
+import subprocess
 
 app = Celery('video_processing', backend='redis://localhost', broker='redis://localhost')
 
 @app.task
 def process_video(video_path):
-    import subprocess
     
-    try:
-        
+    try:    
         subprocess.run(['docker', 'run', '-v', f'{video_path}:/app/input', 'batch-processing', '/app/input/pruebaVideo.mp4'], check=True)
-        .
-        
         return "Video processing complete"
+    
     except subprocess.CalledProcessError as e:
-       
         return f"Error processing video: {e}"
 
 if __name__ == "__main__":
