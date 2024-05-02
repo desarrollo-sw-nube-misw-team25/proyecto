b6cc594e6753baf61703e59f9b4ffa117ac47b87
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-01 23:22:27 -0500

    added necesary files

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import errors
+import subprocess
+app = Flask(__name__)
+
+@app.route("/procesarVideo/<video_id>", methods=["POST"])
+def process_video(video_id):
+   
+    filename = f"{video_id}.mp4"
+    command = f"./videoProcessing.sh  {filename}"
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        response = {
+            'status': 'ok',
+            'output': result.stdout.decode()
+        }
+    except subprocess.CalledProcessError as e:
+        response = {
+            'status': 'error',
+            'error': e.stderr.decode()
+        }
+    
+    return jsonify(response)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
