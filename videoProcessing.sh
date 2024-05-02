dd9984ef141e6eb93f2e615d074ee3b32346ccdf
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-01 23:26:33 -0500

    added aws files intead of gcp, fixed this mistakes

--- videoProcessing.sh
+++ videoProcessing.sh
@@ -1,24 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Check if an argument is provided
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <video_name>"
+    echo "Usage: $0 <video_path>"
     exit 1
 fi
 
-video_name="$1"
-input_video="/home/ubuntu/${video_name}"
-processed_folder="/home/ubuntu"
-output_video="${processed_folder}/${video_name%.*}_processed.mp4"
+input_video="$1"
 
-# Fetch the video from S3
-aws s3 cp "s3://nubeandesgrupocool/unprocessed/${video_name}" "$input_video"
-
-# Check if download was successful
-if [ ! -f "$input_video" ]; then
-    echo "Failed to download video."
-    exit 1
-fi
+# Final filename
+processed_folder="/mnt/nfs/general/processed/"
+output_video="${processed_folder}/$(basename ${input_video%.*}_processed.mp4)"
 
 # Create processed directory if it does not exist
 if [ ! -d "$processed_folder" ]; then
@@ -28,7 +20,6 @@ fi
 initial_frame="./idrl.jpg"
 final_frame="./idrl.jpg"
 
-# Get video dimensions
 video_width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 "$input_video")
 video_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "$input_video")
 
@@ -42,12 +33,3 @@ ffmpeg -i "$input_video" -i "$initial_frame" -i "$final_frame" \
 -c:v libx264 -c:a copy -t 20 "$output_video"
 
 echo "The processed video has been saved as: $output_video"
-
-# Upload processed video to S3
-aws s3 cp "$output_video" "s3://nubeandesgrupocool/processed/"
-
-# Delete local copies of the video
-rm "$input_video"
-rm "$output_video"
-
-echo "Local copies of videos have been deleted."
