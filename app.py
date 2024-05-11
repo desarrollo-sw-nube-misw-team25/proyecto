347579adda53432cb74a28b5c53f3e949ece3978
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-11 18:45:39 -0500

    .

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def process_video(message: pubsub_v1.subscriber.message.Message)->None:
             'status': 'error',
             'error': e.stderr.decode()
         }
-    video_id.ack()
+    message.ack()
 
 streaming_pull_future=subscriber.subscribe(subscription_path,callback=process_video)
 
