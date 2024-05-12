251baf019c09fef5025de035d01c55001552d725
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-12 08:47:41 -0500

    added mod

--- app.py
+++ app.py
@@ -9,23 +9,17 @@ subscription_path=subscriber.subscription_path(project_id,subscription_id)
 
 #@app.route("/procesarVideo/<video_id>", methods=["POST"])
 def process_video(message: pubsub_v1.subscriber.message.Message)->None:
+    print("Video processing starting [+]")
     video_id=message.data.decode("utf-8")
-    print(video_id)
     filename = f"{video_id}.mp4"
     command = f"./videoProcessing.sh  {filename}"
     print(filename)
     try:
         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        response = {
-            'status': 'ok',
-            'output': result.stdout.decode()
-        }
+        print("Video processing  finished succesfully [+]")
+    
     except subprocess.CalledProcessError as e:
-        response = {
-            'status': 'error',
-            'error': e.stderr.decode()
-        }
+        print("Someting went wrong [:(]")
     except  Exception as e:
         raise(e)
     message.ack()
