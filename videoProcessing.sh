9dba2de5d936c117d907d5ace477cf6a8fe1074f
Santiago Hernandez <s.hernandezf@uniandes.edu.co>
2024-05-26 21:53:43 -0500

    aa

--- videoProcessing.sh
+++ videoProcessing.sh
@@ -11,7 +11,7 @@ input_video="./${video_name}"
 processed_folder="."
 output_video="${processed_folder}/${video_name%.*}_processed.mp4"
 
-sudo gsutil cp "gs://almacenamiento2-videos-nube/unprocessed/${video_name}" "${input_video}"
+gsutil cp "gs://almacenamiento2-videos-nube/unprocessed/${video_name}" "${input_video}"
 
 # Revisar si se descargo bien
 if [ ! -f "$input_video" ]; then
@@ -37,7 +37,7 @@ ffmpeg -i "$input_video" -i "$initial_frame" -i "$final_frame" \
 
 echo "The processed video has been saved as: $output_video"
 
-sudo gsutil cp "${output_video}" "gs://almacenamiento2-videos-nube/processed/" 
+gsutil cp "${output_video}" "gs://almacenamiento2-videos-nube/processed/" 
 
 rm -f "$input_video"
 rm -f "$output_video"
